a3ba85be74089b625bc41a71c3516c2ed5df4b18
PXgamer <owzie123@gmail.com>
2017-03-08 14:07:10 +0000

    Updated Dashboard

--- src/paths/dashboard.php
+++ src/paths/dashboard.php
@@ -1,23 +1,20 @@
 <?php
-  session_start();
-  require_once("application/functions/config.php");
-  if(!isset($_SESSION['passwordPanel']) OR $_SESSION['passwordPanel'] != PASSWORDPANEL OR $_SESSION['IPOrigin'] != $_SERVER['REMOTE_ADDR']):
+session_start();
+require_once("application/functions/config.php");
+if (!isset($_SESSION['passwordPanel']) OR $_SESSION['passwordPanel'] != PASSWORDPANEL OR $_SESSION['IPOrigin'] != $_SERVER['REMOTE_ADDR']):
     header("Location: administration");
-  endif;
+endif;
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <?=get_head()?>
+<head>
+    <?= \gabrielcarvalhogama\MagTorrents\Inclusions\Head::get() ?>
     <title>MagTorrents - Dashboard</title>
-
-    <link rel="stylesheet" href="lib/css/bootstrap.css" />
-    <link rel="stylesheet" href="lib/css/dashboard.css" />
-  </head>
-  <body>
-    <?=get_nav_adm()?>
-    <main>
-      Estatísticas ou sla
-    </main>
-  </body>
+</head>
+<body>
+<?= \gabrielcarvalhogama\MagTorrents\Inclusions\Navbar::get() ?>
+<main>
+    Estatísticas ou sla
+</main>
+</body>
 </html>
