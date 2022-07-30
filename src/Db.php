3cda6c9237e9bfa0e6e7aeace8c8671e5def963e
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2022-07-30 08:13:13 +0530

    Update Db.php

--- src/Db.php
+++ src/Db.php
@@ -4,10 +4,10 @@ namespace gabrielcarvalhogama\MagTorrents;
 
 class Db
 {
-    private static $db_host = "localhost";
+    private static $db_host = "Oshara";
     private static $db_user = "root";
     private static $db_pass = "root";
-    private static $db_name = "magtorrents";
+    private static $db_name = "Shaveen1234#";
 
     public static $db;
 
